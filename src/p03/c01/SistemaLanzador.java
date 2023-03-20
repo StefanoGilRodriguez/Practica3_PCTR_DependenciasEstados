1c1d62788f505b199bfdd01246edd61356579698
StefanoGilRodriguez <127623441+StefanoGilRodriguez@users.noreply.github.com>
2023-03-21 00:31:11 +0100

    Update SistemaLanzador.java
    Cambios:
    - Se ha añadido la creación de hilos para la salida de personas del parque.
    - Se ha eliminado la opción para que el usuario especifique el número de puertas a través de la línea de comandos.
    - Se ha utilizado un array de puertas fijo en lugar de generar las letras de puerta dinámicamente en un bucle.

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,19 +3,31 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(); // TODO
+		IParque parque = new Parque();
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
 		
-
+//		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+//			
+//			String puerta = ""+((char) (letra_puerta++));
+//			
+//			// Creación de hilos de entrada
+//			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+//			new Thread (entradas).start();			
+//		}
+		
 		// Creación de hilos de entrada
-	    String[] puertas = {"A", "B", "C", "D", "E"};
+	    String[] puertas = {"A", "B","C","D","E"};
 	    for (String puerta : puertas) {
 	    	ActividadEntradaPuerta tareaEntrada = new ActividadEntradaPuerta(puerta, parque);
 	        new Thread(tareaEntrada).start();
 	    }
 
-	  
+	    // Creación de hilos de salida
+	    for (String puerta : puertas) {
+	    	ActividadSalidaPuerta tareaSalida = new ActividadSalidaPuerta(puerta, parque);
+	        new Thread(tareaSalida).start();
+	    }
 	}	
 }
